baf01871f9f7b7fe780b840f21f8cd81f5310bc8
yunyin <yunyin_jayyi@qq.com>
2023-10-03 10:53:32 +0800

    update on 2023-10-03

--- rewrite.sh
+++ rewrite.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# 列出需要恢复修改时间的文件
-find . -name '*.md' | while read file; do touch -d "$(git log -1 --pretty="@%ct" -- "$file")" "$file";
+# 列出需要恢复修改时间的.md文件
+find . -name '*.md' | while read file; do
+    touch -d "$(git log -1 --pretty="@%ct" -- "$file")" "$file";
+done;
+
+# 列出需要恢复修改时间的.xmind文件
+find . -name '*.xmind' | while read file; do
+    touch -d "$(git log -1 --pretty="@%ct" -- "$file")" "$file";
 done;
