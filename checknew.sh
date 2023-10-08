b6ef95be03f64e20e9e7b7a8d61d267bfe939870
yunyin <yunyin_jayyi@qq.com>
2023-10-08 12:27:08 +0800

    update on 2023-10-08

--- checknew.sh
+++ checknew.sh
@@ -2,11 +2,11 @@
 git fetch
 
 # 检查本地的main分支和远程的main分支是否有差异
-if git diff --quiet main origin/main; then
+if git diff --quiet master origin/master; then
   echo "远程main分支没有更新，不执行后续操作"
   exit 0
 else
   echo "远程main分支有更新，本地执行更新"
-  git pull origin main
+  git pull origin master
   exit 1
 fi
